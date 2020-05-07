7d0feb7904653c136ef77592aa49b7c9dea65038
weiiiiyu <weiiiiyu@gmail.com>
2020-05-08 00:07:24 +0800

    5_8
    finger angle

--- c/test.cs
+++ c/test.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap.Unity;
 
 public class test : MonoBehaviour
 {
+    public float testf;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,11 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        testf = GetComponent<RigidFinger>().GetFingerJointStretchMecanim(1);
+        if(testf>-10)
+        {
+            Debug.Log("one");
+        }
+    
     }
 }
